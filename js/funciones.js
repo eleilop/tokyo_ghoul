e46921d1bb622d07fab23c470ae8cb4ca6d9d2e6
eleilop <118524344+eleilop@users.noreply.github.com>
2022-12-08 18:13:32 +0100

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,11 @@
+/************* JS QUE REGULA LA BARRA DE NAVEGACIÓN *************/
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
